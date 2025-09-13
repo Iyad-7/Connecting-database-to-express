e2ce4b46e34c196e6fd0fb64ffe77c9569008301
Iyad-7 <iyadabdulrahman6@gmail.com>
2025-09-13 12:10:27 +0530

    Connected to Express

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+app.use(express.json());
+const mongoUri = process.env.MONGO_URI||'mongodb://localhost:27017/inventoryDB';
 
+const port = process.env.PORT || 3000;
+mongoose.connect(mongoUri, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+ })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log("Error connecting to MongoDB:", err));
 app.use(express.static('static'));
+ 
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
